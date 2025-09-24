1c1b235d5efbf06e8e9a3b994310b943ab40fff0
PadoleKiran <124488404+PadoleKiran@users.noreply.github.com>
2025-09-24 11:00:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//your JS code here. If required.
-const changeButton = document.getElementById("change_button");
-    const resetButton = document.getElementById("Reset");
+ const changeButton = document.getElementById("change_button");
+    const resetButton = document.getElementById("reset_button");
     const blockInput = document.getElementById("block_id");
     const colorInput = document.getElementById("colour_id");
     const gridItems = document.querySelectorAll(".grid-item");
